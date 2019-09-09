4af6b50d8fc221374b6610fe9f2d8df5b8b93191
viktorfrom <viktor.from91@gmail.com>
2019-09-09 09:12:34 +0200

    Change main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-mod parser;
-mod tree;
+extern crate nom;
 
-pub use crate::parser::stringparser;
-pub use crate::tree::treebuilder;
+use nom::{
+    character::complete::{multispace0, digit1},
+    IResult,
+    sequence::{preceded},
+    bytes::complete::tag,
+    branch::alt,
+};
+
+fn parse_expr(string: &str) -> IResult <&str, &str> {
+    let a: IResult<&str, &str> = preceded(multispace0, 
+            alt((tag("+"), digit1)
+            ))(string);
+    return a
+
+}
 
 fn main() {
-    let string = "1 + 2 + 1";
-    
-    let parsed = stringparser::try_parse(string);
-    println!("{:#?}", parsed);
+    let string = "   1 + 2 + 1";
 
-    // let tree = treebuilder::build_tree("+", 1, 2);
-    // println!("{:#?}", tree);
+
+    let res = parse_expr(string);
+    println!("{:?}", res.unwrap().1);
+}
+
+#[derive(Debug)]
+pub enum Tree {
+    Root(char, Box<Tree>, Box<Tree>),
+    Leaf(i32),
+}
+
+fn build_tree(operator: &str, left: i32, right: i32) -> Tree { // ska vara Box<Tree> ist for i32 vid recursion
+    let tree = Tree::Root(operator.parse().unwrap(), 
+                    Box::new(Tree::Leaf(left)), 
+                    Box::new(Tree::Leaf(right)));
+    return tree;
 }
