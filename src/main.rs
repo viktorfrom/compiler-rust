a2842dbafa90b7079e0f240a44550e19378e3747
viktorfrom <viktor.from91@gmail.com>
2019-09-20 08:32:42 +0200

    Add Logcal Op

--- src/main.rs
+++ src/main.rs
@@ -5,56 +5,137 @@ use nom::{
     bytes::complete::tag,
     character::complete::{digit1, multispace0},
     combinator::map,
-    sequence::preceded,
+    sequence::delimited,
     IResult,
 };
 
 #[derive(Debug)]
-pub enum Node {
-    Root(Op, Box<Node>, Box<Node>),
-    Leaf(i32),
+pub enum Tree {
+    AriNode(AriOp, Box<Tree>, Box<Tree>),
+    LogNode(LogOp, Box<Tree>, Box<Tree>),
+    Num(i32),
+    Bool(Bool),
+    Id(String),
+    // UnOp(Op, Box<Tree>),
+    // Application(Id, vec<Tree>)
 }
 
 #[derive(Debug)]
-pub enum Op {
+pub enum AriOp {
     Add,
     Sub,
+    Mult,
+    Div,
 }
 
-fn parse_op(input: &str) -> IResult<&str, Op> {
-    preceded(
+#[derive(Debug)]
+pub enum Bool {
+    True,
+    False,
+}
+
+#[derive(Debug)]
+pub enum LogOp {
+    And,
+    Or,
+}
+
+fn parse_binOp(input: &str) -> IResult<&str, LogOp> {
+    delimited(
         multispace0,
-        alt((map(tag("+"), |_| Op::Add), map(tag("-"), |_| Op::Sub))),
+        alt((
+            map(tag("&&"), |_| LogOp::And),
+            map(tag("||"), |_| LogOp::Or),
+        )),
+        multispace0,
+    )(input)
+}
+
+fn parse_bool(input: &str) -> IResult<&str, Bool> {
+    delimited(
+        multispace0,
+        alt((
+            map(tag("true"), |_| Bool::True),
+            map(tag("false"), |_| Bool::False),
+        )),
+        multispace0,
+    )(input)
+}
+
+fn parse_op(input: &str) -> IResult<&str, AriOp> {
+    delimited(
+        multispace0,
+        alt((
+            map(tag("+"), |_| AriOp::Add),
+            map(tag("-"), |_| AriOp::Sub),
+            map(tag("*"), |_| AriOp::Mult),
+            map(tag("/"), |_| AriOp::Div),
+        )),
+        multispace0,
     )(input)
 }
 
 fn parse_i32(input: &str) -> IResult<&str, i32> {
-    let (substring, digit) = preceded(multispace0, digit1)(input)?;
+    let (substring, digit) = delimited(multispace0, digit1, multispace0)(input)?;
 
     Ok((substring, digit.parse::<i32>().unwrap()))
 }
 
-fn build_tree(string: &str) -> IResult<&str, Node> {
+// for interpreter
+// fn eval_type(input: Tree) -> Tree {
+//     match input {
+//     input => Num(i32),
+//     _ => println!("something else"),
+// }
+
+
+fn build_AriTree(string: &str) -> IResult<&str, Tree> {
     let (substring, digit) = parse_i32(string)?;
-    if substring == "" {
-        return Ok(("", Node::Leaf(digit)));
+    if substring == ";" {
+        return Ok(("", Tree::Num(digit)));
     } else {
         let op = parse_op(substring).unwrap().1;
         let right = parse_op(substring).unwrap().0;
 
-        let tree = Node::Root(
+        let tree = Tree::AriNode(
             op,
-            Box::new(Node::Leaf(digit)),
-            Box::new(build_tree(right).unwrap().1),
+            Box::new(Tree::Num(digit)),
+            Box::new(build_AriTree(right).unwrap().1),
         );
 
         return Ok(("", tree));
     }
 }
 
+fn build_LogTree(string: &str) -> IResult<&str, Tree> {
+    let (substring, boolean) = parse_bool(string)?;
+    if substring == ";" {
+        return Ok(("", Tree::Bool(boolean)));
+    } else {
+        let op = parse_binOp(substring).unwrap().1;
+        let right = parse_binOp(substring).unwrap().0;
+
+        let tree = Tree::LogNode(
+            op,
+            Box::new(Tree::Bool(boolean)),
+            Box::new(build_LogTree(right).unwrap().1),
+        );
+
+        return Ok(("", tree));
+    }
+}
+
+fn parse_expr(input: &str) -> IResult<&str, Tree> {
+    alt((
+        build_AriTree,
+        build_LogTree
+    ))(input)
+}
+// cat(tag(")")) for att parsa "("                  ")"
 fn main() {
-    let string = "        1 + 2 +1";
-    let tree = build_tree(string);
+    // let string = "        1 + 2 -1 / 5     *      3       ;";
+    let string = "            true && false ||           true       ;";
+    let tree = parse_expr(string);
     // println!("{:#?}", tree);
     println!("{:#?}", tree);
 }
