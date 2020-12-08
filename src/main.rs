b8b6c2491951799626b4a2e248145dd35ff616ee
Viktor From <viktor.from91@gmail.com>
2020-12-08 19:51:39 +0100

    Update main

--- src/main.rs
+++ src/main.rs
@@ -41,61 +41,31 @@ fn main() {
     // }
 
     // LLVM
-    let program1 = " 
-        fn testfn() -> i32 {
+    let program = " 
+        fn testfn1() -> i32 {
             let a: i32 = 1;
             let b: i32 = a;
             let c: i32 = b;
             return c;
         };
-        ";
 
-    let program2 = " 
-        fn testfn(c: i32) -> i32 {
-            let a: i32 = c;
-            return a;
+        fn testfn2() -> i32 {
+            let b: bool = true && true;
+            if b {
+                return 50;
+            };
+            return 1;
         };
 
         fn main() -> i32 {
-            let b: i32 = testfn(3);
-            return b;
-        };
-
-        let res: i32 = main();
-        ";
-
-    let program3 = " 
-        fn testfn() -> i32 {
-            let b: bool = true && true;
-            if b {
-                return 50;
-            };
-            return 1;
-        };
-        ";
-
-    let program4 = " 
-        fn testfn2() -> i32 {
-            return 2;
-        };
-
-        fn testfn3() -> i32 {
-            let b: bool = true && true;
-            if b {
-                return 50;
-            };
-            return 1;
-        };
-
-        fn testfn() -> i32 {
-            let a: i32 = testfn2(); 
-            let b: i32 = testfn3();
+            let a: i32 = testfn1(); 
+            let b: i32 = testfn2();
             return b;
         };
         ";
 
 
-    let tree = parse_expr(program4).unwrap().1;
+    let tree = parse_expr(program).unwrap().1;
     println!("Tree = {:#?}", tree);
 
     if type_scope(tree.clone()) {
