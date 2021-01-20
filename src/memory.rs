cdc3ff2b25dfc2668d31107b0dd156aa4ccf3f06
Viktor From <viktor.from91@gmail.com>
2021-01-20 12:50:52 +0100

    Remove unused fn

--- src/memory.rs
+++ src/memory.rs
@@ -6,7 +6,7 @@ use lazy_static;
 use std::sync::Mutex;
 #[derive(Debug, PartialEq, Clone)]
 pub enum ExprRep {
-    Int(i32),
+    Int(&i32),
     Var(String),
     Bool(bool),
 
@@ -78,31 +78,4 @@ pub fn read_fn(key: &str) -> ExprRep {
             panic!("Could not find fn {:#?} in map", key)
         }
     }
-}
-
-pub fn push_on_return_stack(val: ExprRep) {
-    let mut stack = STACK.lock().unwrap();
-    // println!("stack = {:#?}", stack);
-    stack.push(val);
-}
-
-pub fn pop_from_return_stack() -> ExprRep {
-    let mut stack = STACK.lock().unwrap();
-    match stack.pop() {
-        Some(i) => i,
-        None => ExprRep::Null,
-    }
-}
-
-pub fn push_on_mem_stack() {
-    let mut stack = SCOPE.lock().unwrap();
-
-    stack.push(Mutex::new(HashMap::new()));
-}
-
-pub fn pop_from_mem_stack() {
-    let mut stack = SCOPE.lock().unwrap();
-    println!("SKOJJAR DU????+");
-
-    stack.pop();
 }
